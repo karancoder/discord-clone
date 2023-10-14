773c4c04e65379f6171643178fe46e557cef3d7e
karancoder <jivanikaran@gmail.com>
2023-10-15 02:36:56 +0530

    chore: add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main>
+      <Button>Hello world!</Button>
       <p className="text-3xl font-bold text-indigo-400">Hello World!</p>
     </main>
   );
