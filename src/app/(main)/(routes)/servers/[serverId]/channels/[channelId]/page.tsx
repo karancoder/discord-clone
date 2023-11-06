a9288272f145cadcc28453c11af2a25c9749d665
karancoder <jivanikaran@gmail.com>
2023-11-06 22:41:44 +0530

    chat messages

--- src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ src/app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,5 +1,6 @@
 import ChatHeader from "@/components/chat/chat-header";
 import ChatInput from "@/components/chat/chat-input";
+import ChatMessages from "@/components/chat/chat-messages";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
@@ -44,7 +45,17 @@ const ChannelIDPage = async ({ params }: ChannelIDPageProps) => {
         serverId={channel.serverId}
         type={"channel"}
       />
-      <div className="flex-1">Future Messages</div>
+      <ChatMessages
+        member={member}
+        name={channel.name}
+        type="channel"
+        chatId={channel.id}
+        apiUrl="/api/messages"
+        socketUrl="/api/socket/messages"
+        socketQuery={{ channelId: channel.id, serverId: params.serverId }}
+        paramKey="channelId"
+        paramValue={channel.id}
+      />
       <ChatInput
         name={channel.name}
         type="channel"
---
+++ src/hooks/use-chat-query.ts
@@ -0,0 +1,46 @@
+import { useSocket } from "@/components/providers/socket-provider";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import qs from "query-string";
+
+type ChatQueryProps = {
+  queryKey: string;
+  apiUrl: string;
+  paramKey: "channelId" | "conversationId";
+  paramValue: string;
+};
+
+export default function useChatQuery({
+  queryKey,
+  apiUrl,
+  paramKey,
+  paramValue,
+}: ChatQueryProps) {
+  const { isConnected } = useSocket();
+
+  const fetchMessages = async ({ pageParam = undefined }) => {
+    const url = qs.stringifyUrl(
+      {
+        url: apiUrl,
+        query: {
+          cursor: pageParam,
+          [paramKey]: paramValue,
+        },
+      },
+      { skipNull: true }
+    );
+
+    const res = await fetch(url);
+    return res.json();
+  };
+
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
+    useInfiniteQuery({
+      queryKey: [queryKey],
+      queryFn: fetchMessages,
+      getNextPageParam: (lastPage_) => lastPage_?.nextCursor,
+      refetchInterval: isConnected ? false : 1000,
+      initialPageParam: undefined,
+    });
+
+  return { data, fetchNextPage, hasNextPage, isFetchingNextPage, status };
+}
